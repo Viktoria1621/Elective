1528421eadc9e708db97a0b7980ae02283d3f1cc
Viktoria <89690905+Viktoria1621@users.noreply.github.com>
2022-02-22 20:10:12 +0300

    Delete main/java/by/tc/elective/dao directory

--- main/java/by/tc/elective/dao/QuestionTransactionDAO.java
+++
@@ -1,9 +0,0 @@
-package by.tc.elective.dao;
-
-import by.tc.elective.bean.Answer;
-import by.tc.elective.bean.Question;
-
-public interface QuestionTransactionDAO {
-    void initQuestion(Question newQuestion, Answer newAnswer) throws DAOException;
-    void deleteQuestion(int id) throws DAOException;
-}
--- main/java/by/tc/elective/dao/pool/ConnectionPoolException.java
+++
@@ -1,9 +0,0 @@
-package by.tc.elective.dao.pool;
-
-public class ConnectionPoolException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public ConnectionPoolException (String massage, Exception e) {
-        super(massage, e);
-    }
-}
